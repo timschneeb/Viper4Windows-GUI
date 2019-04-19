a89419b9ac6d4e2f71adcfc84e7360402c54cf06
ThePBone <thebone.main@gmail.com>
2019-04-19 04:41:04 +0200

    Migrated Check Update Dialog

--- ViPER4WindowsCtrlPanel/frmCheckUpdate.Designer.cs
+++ ViPER4WindowsCtrlPanel/frmCheckUpdate.Designer.cs
@@ -16,96 +16,141 @@
 		// Token: 0x0600025A RID: 602 RVA: 0x0001EE54 File Offset: 0x0001D054
 		private void InitializeComponent()
 		{
-			global::System.ComponentModel.ComponentResourceManager componentResourceManager = new global::System.ComponentModel.ComponentResourceManager(typeof(global::ViPER4WindowsBin.frmCheckUpdate));
-			this.label_CodeName = new global::System.Windows.Forms.Label();
-			this.label_DriverVersion = new global::System.Windows.Forms.Label();
-			this.label_ControlPanelVersion = new global::System.Windows.Forms.Label();
-			this.groupBox_VersionFromInternet = new global::System.Windows.Forms.GroupBox();
-			this.label_CodeName_Net = new global::System.Windows.Forms.Label();
-			this.label_DriverVerion_Net = new global::System.Windows.Forms.Label();
-			this.label_ControlPanelVersion_Net = new global::System.Windows.Forms.Label();
-			this.singleButton_Check = new global::ViPER4WindowsBin.UIControls.SingleButton();
-			this.singleButton_Copy = new global::ViPER4WindowsBin.UIControls.SingleButton();
-			this.groupBox_VersionFromInternet.SuspendLayout();
-			base.SuspendLayout();
-			this.label_CodeName.AutoSize = true;
-			this.label_CodeName.Location = new global::System.Drawing.Point(12, 63);
-			this.label_CodeName.Name = "label_CodeName";
-			this.label_CodeName.Size = new global::System.Drawing.Size(65, 12);
-			this.label_CodeName.TabIndex = 5;
-			this.label_CodeName.Text = "$CODE_NAME";
-			this.label_DriverVersion.AutoSize = true;
-			this.label_DriverVersion.Location = new global::System.Drawing.Point(12, 41);
-			this.label_DriverVersion.Name = "label_DriverVersion";
-			this.label_DriverVersion.Size = new global::System.Drawing.Size(95, 12);
-			this.label_DriverVersion.TabIndex = 4;
-			this.label_DriverVersion.Text = "$DRIVER_VERSION";
-			this.label_ControlPanelVersion.AutoSize = true;
-			this.label_ControlPanelVersion.Location = new global::System.Drawing.Point(12, 19);
-			this.label_ControlPanelVersion.Name = "label_ControlPanelVersion";
-			this.label_ControlPanelVersion.Size = new global::System.Drawing.Size(131, 12);
-			this.label_ControlPanelVersion.TabIndex = 3;
-			this.label_ControlPanelVersion.Text = "$CONTROLPANEL_VERSION";
-			this.groupBox_VersionFromInternet.Controls.Add(this.label_CodeName_Net);
-			this.groupBox_VersionFromInternet.Controls.Add(this.label_DriverVerion_Net);
-			this.groupBox_VersionFromInternet.Controls.Add(this.label_ControlPanelVersion_Net);
-			this.groupBox_VersionFromInternet.Location = new global::System.Drawing.Point(14, 97);
-			this.groupBox_VersionFromInternet.Name = "groupBox_VersionFromInternet";
-			this.groupBox_VersionFromInternet.Size = new global::System.Drawing.Size(332, 96);
-			this.groupBox_VersionFromInternet.TabIndex = 6;
-			this.groupBox_VersionFromInternet.TabStop = false;
-			this.groupBox_VersionFromInternet.Text = "$VERSION_FROM_INTERNET";
-			this.label_CodeName_Net.AutoSize = true;
-			this.label_CodeName_Net.Location = new global::System.Drawing.Point(17, 70);
-			this.label_CodeName_Net.Name = "label_CodeName_Net";
-			this.label_CodeName_Net.Size = new global::System.Drawing.Size(65, 12);
-			this.label_CodeName_Net.TabIndex = 8;
-			this.label_CodeName_Net.Text = "$CODE_NAME";
-			this.label_DriverVerion_Net.AutoSize = true;
-			this.label_DriverVerion_Net.Location = new global::System.Drawing.Point(17, 48);
-			this.label_DriverVerion_Net.Name = "label_DriverVerion_Net";
-			this.label_DriverVerion_Net.Size = new global::System.Drawing.Size(95, 12);
-			this.label_DriverVerion_Net.TabIndex = 7;
-			this.label_DriverVerion_Net.Text = "$DRIVER_VERSION";
-			this.label_ControlPanelVersion_Net.AutoSize = true;
-			this.label_ControlPanelVersion_Net.Location = new global::System.Drawing.Point(17, 26);
-			this.label_ControlPanelVersion_Net.Name = "label_ControlPanelVersion_Net";
-			this.label_ControlPanelVersion_Net.Size = new global::System.Drawing.Size(131, 12);
-			this.label_ControlPanelVersion_Net.TabIndex = 6;
-			this.label_ControlPanelVersion_Net.Text = "$CONTROLPANEL_VERSION";
-			this.singleButton_Check.BackColor = global::System.Drawing.SystemColors.Control;
-			this.singleButton_Check.ButtonText = "$CHECK";
-			this.singleButton_Check.Location = new global::System.Drawing.Point(239, 19);
-			this.singleButton_Check.Name = "singleButton_Check";
-			this.singleButton_Check.Size = new global::System.Drawing.Size(112, 40);
-			this.singleButton_Check.TabIndex = 7;
-			this.singleButton_Check.ButtonClickNotify += new global::ViPER4WindowsBin.UIControls.SingleButton.ButtonClickEventDelegate(this.singleButton_Check_ButtonClickNotify);
-			this.singleButton_Copy.BackColor = global::System.Drawing.SystemColors.Control;
-			this.singleButton_Copy.ButtonText = "$COPY_DOWNLOAD_LINK";
-			this.singleButton_Copy.Location = new global::System.Drawing.Point(14, 199);
-			this.singleButton_Copy.Name = "singleButton_Copy";
-			this.singleButton_Copy.Size = new global::System.Drawing.Size(166, 40);
-			this.singleButton_Copy.TabIndex = 8;
-			this.singleButton_Copy.ButtonClickNotify += new global::ViPER4WindowsBin.UIControls.SingleButton.ButtonClickEventDelegate(this.singleButton_Copy_ButtonClickNotify);
-			base.AutoScaleDimensions = new global::System.Drawing.SizeF(6f, 12f);
-			base.AutoScaleMode = global::System.Windows.Forms.AutoScaleMode.Font;
-			base.ClientSize = new global::System.Drawing.Size(363, 250);
-			base.Controls.Add(this.singleButton_Copy);
-			base.Controls.Add(this.singleButton_Check);
-			base.Controls.Add(this.groupBox_VersionFromInternet);
-			base.Controls.Add(this.label_CodeName);
-			base.Controls.Add(this.label_DriverVersion);
-			base.Controls.Add(this.label_ControlPanelVersion);
-			this.DoubleBuffered = true;
-			base.FormBorderStyle = global::System.Windows.Forms.FormBorderStyle.FixedSingle;
-			base.MaximizeBox = false;
-			base.Name = "frmCheckUpdate";
-			base.StartPosition = global::System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "$CHECK_UPDATE";
-			this.groupBox_VersionFromInternet.ResumeLayout(false);
-			this.groupBox_VersionFromInternet.PerformLayout();
-			base.ResumeLayout(false);
-			base.PerformLayout();
+            this.components = new System.ComponentModel.Container();
+            this.label_CodeName = new System.Windows.Forms.Label();
+            this.label_DriverVersion = new System.Windows.Forms.Label();
+            this.label_ControlPanelVersion = new System.Windows.Forms.Label();
+            this.groupBox_VersionFromInternet = new System.Windows.Forms.GroupBox();
+            this.label_CodeName_Net = new System.Windows.Forms.Label();
+            this.label_DriverVerion_Net = new System.Windows.Forms.Label();
+            this.label_ControlPanelVersion_Net = new System.Windows.Forms.Label();
+            this.singleButton_Check = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.skinManager1 = new Syncfusion.Windows.Forms.SkinManager(this.components);
+            this.singleButton_Copy = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.groupBox_VersionFromInternet.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_CodeName
+            // 
+            this.label_CodeName.AutoSize = true;
+            this.label_CodeName.Location = new System.Drawing.Point(12, 68);
+            this.label_CodeName.Name = "label_CodeName";
+            this.label_CodeName.Size = new System.Drawing.Size(80, 13);
+            this.label_CodeName.TabIndex = 5;
+            this.label_CodeName.Text = "$CODE_NAME";
+            // 
+            // label_DriverVersion
+            // 
+            this.label_DriverVersion.AutoSize = true;
+            this.label_DriverVersion.Location = new System.Drawing.Point(12, 44);
+            this.label_DriverVersion.Name = "label_DriverVersion";
+            this.label_DriverVersion.Size = new System.Drawing.Size(108, 13);
+            this.label_DriverVersion.TabIndex = 4;
+            this.label_DriverVersion.Text = "$DRIVER_VERSION";
+            // 
+            // label_ControlPanelVersion
+            // 
+            this.label_ControlPanelVersion.AutoSize = true;
+            this.label_ControlPanelVersion.Location = new System.Drawing.Point(12, 21);
+            this.label_ControlPanelVersion.Name = "label_ControlPanelVersion";
+            this.label_ControlPanelVersion.Size = new System.Drawing.Size(154, 13);
+            this.label_ControlPanelVersion.TabIndex = 3;
+            this.label_ControlPanelVersion.Text = "$CONTROLPANEL_VERSION";
+            // 
+            // groupBox_VersionFromInternet
+            // 
+            this.groupBox_VersionFromInternet.Controls.Add(this.label_CodeName_Net);
+            this.groupBox_VersionFromInternet.Controls.Add(this.label_DriverVerion_Net);
+            this.groupBox_VersionFromInternet.Controls.Add(this.label_ControlPanelVersion_Net);
+            this.groupBox_VersionFromInternet.Location = new System.Drawing.Point(14, 105);
+            this.groupBox_VersionFromInternet.Name = "groupBox_VersionFromInternet";
+            this.groupBox_VersionFromInternet.Size = new System.Drawing.Size(332, 104);
+            this.groupBox_VersionFromInternet.TabIndex = 6;
+            this.groupBox_VersionFromInternet.TabStop = false;
+            this.groupBox_VersionFromInternet.Text = "$VERSION_FROM_INTERNET";
+            // 
+            // label_CodeName_Net
+            // 
+            this.label_CodeName_Net.AutoSize = true;
+            this.label_CodeName_Net.Location = new System.Drawing.Point(17, 76);
+            this.label_CodeName_Net.Name = "label_CodeName_Net";
+            this.label_CodeName_Net.Size = new System.Drawing.Size(80, 13);
+            this.label_CodeName_Net.TabIndex = 8;
+            this.label_CodeName_Net.Text = "$CODE_NAME";
+            // 
+            // label_DriverVerion_Net
+            // 
+            this.label_DriverVerion_Net.AutoSize = true;
+            this.label_DriverVerion_Net.Location = new System.Drawing.Point(17, 52);
+            this.label_DriverVerion_Net.Name = "label_DriverVerion_Net";
+            this.label_DriverVerion_Net.Size = new System.Drawing.Size(108, 13);
+            this.label_DriverVerion_Net.TabIndex = 7;
+            this.label_DriverVerion_Net.Text = "$DRIVER_VERSION";
+            // 
+            // label_ControlPanelVersion_Net
+            // 
+            this.label_ControlPanelVersion_Net.AutoSize = true;
+            this.label_ControlPanelVersion_Net.Location = new System.Drawing.Point(17, 28);
+            this.label_ControlPanelVersion_Net.Name = "label_ControlPanelVersion_Net";
+            this.label_ControlPanelVersion_Net.Size = new System.Drawing.Size(154, 13);
+            this.label_ControlPanelVersion_Net.TabIndex = 6;
+            this.label_ControlPanelVersion_Net.Text = "$CONTROLPANEL_VERSION";
+            // 
+            // singleButton_Check
+            // 
+            this.singleButton_Check.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Office2016Colorful;
+            this.singleButton_Check.BeforeTouchSize = new System.Drawing.Size(160, 34);
+            this.singleButton_Check.IsBackStageButton = false;
+            this.singleButton_Check.Location = new System.Drawing.Point(186, 216);
+            this.singleButton_Check.Name = "singleButton_Check";
+            this.singleButton_Check.Size = new System.Drawing.Size(160, 34);
+            this.singleButton_Check.TabIndex = 9;
+            this.singleButton_Check.Text = "$CHECK";
+            this.singleButton_Check.ThemeName = "Office2016Colorful";
+            this.singleButton_Check.UseVisualStyle = true;
+            this.singleButton_Check.Click += new System.EventHandler(this.SingleButton_Check_Click);
+            // 
+            // skinManager1
+            // 
+            this.skinManager1.Controls = this;
+            this.skinManager1.VisualTheme = Syncfusion.Windows.Forms.VisualTheme.Office2016Colorful;
+            // 
+            // singleButton_Copy
+            // 
+            this.singleButton_Copy.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Office2016Colorful;
+            this.singleButton_Copy.BeforeTouchSize = new System.Drawing.Size(160, 34);
+            this.singleButton_Copy.IsBackStageButton = false;
+            this.singleButton_Copy.Location = new System.Drawing.Point(15, 216);
+            this.singleButton_Copy.Name = "singleButton_Copy";
+            this.singleButton_Copy.Size = new System.Drawing.Size(160, 34);
+            this.singleButton_Copy.TabIndex = 10;
+            this.singleButton_Copy.Text = "$COPY";
+            this.singleButton_Copy.ThemeName = "Office2016Colorful";
+            this.singleButton_Copy.UseVisualStyle = true;
+            this.singleButton_Copy.Click += new System.EventHandler(this.SingleButton_Copy_Click);
+            // 
+            // frmCheckUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 265);
+            this.Controls.Add(this.singleButton_Copy);
+            this.Controls.Add(this.singleButton_Check);
+            this.Controls.Add(this.groupBox_VersionFromInternet);
+            this.Controls.Add(this.label_CodeName);
+            this.Controls.Add(this.label_DriverVersion);
+            this.Controls.Add(this.label_ControlPanelVersion);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmCheckUpdate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "$CHECK_UPDATE";
+            this.groupBox_VersionFromInternet.ResumeLayout(false);
+            this.groupBox_VersionFromInternet.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
 		// Token: 0x040002B9 RID: 697
@@ -131,11 +176,8 @@
 
 		// Token: 0x040002C0 RID: 704
 		private global::System.Windows.Forms.Label label_ControlPanelVersion_Net;
-
-		// Token: 0x040002C1 RID: 705
-		private global::ViPER4WindowsBin.UIControls.SingleButton singleButton_Check;
-
-		// Token: 0x040002C2 RID: 706
-		private global::ViPER4WindowsBin.UIControls.SingleButton singleButton_Copy;
-	}
+        private Syncfusion.Windows.Forms.ButtonAdv singleButton_Check;
+        private Syncfusion.Windows.Forms.SkinManager skinManager1;
+        private Syncfusion.Windows.Forms.ButtonAdv singleButton_Copy;
+    }
 }
--- ViPER4WindowsCtrlPanel/frmCheckUpdate.cs
+++ ViPER4WindowsCtrlPanel/frmCheckUpdate.cs
@@ -34,125 +34,124 @@ namespace ViPER4WindowsBin
 			this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
 			this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
 			this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-			this.singleButton_Check.ButtonText = GlobalMessages.CHECK;
-			this.singleButton_Copy.ButtonText = GlobalMessages.COPY_DOWNLOAD_LINK;
+			this.singleButton_Check.Text = GlobalMessages.CHECK;
+			this.singleButton_Copy.Text = GlobalMessages.COPY_DOWNLOAD_LINK;
 			this.singleButton_Copy.Enabled = false;
 		}
 
-		// Token: 0x06000257 RID: 599 RVA: 0x0001E9F4 File Offset: 0x0001CBF4
-		private void singleButton_Check_ButtonClickNotify(SingleButton objSender)
-		{
-			string text = AppDomain.CurrentDomain.BaseDirectory;
-			if (!text.EndsWith("\\"))
-			{
-				text += "\\version.txt";
-			}
-			else
-			{
-				text += "version.txt";
-			}
-			try
-			{
-				this.singleButton_Check.Enabled = false;
-				HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://vipersaudio.com/swupdate/viper4windows/version.bin");
-				HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-				Stream responseStream = httpWebResponse.GetResponseStream();
-				Stream stream = new FileStream(text, FileMode.Create);
-				byte[] array = new byte[1024];
-				for (int i = responseStream.Read(array, 0, array.Length); i > 0; i = responseStream.Read(array, 0, array.Length))
-				{
-					stream.Write(array, 0, i);
-				}
-				stream.Flush();
-				stream.Close();
-				responseStream.Close();
-				httpWebResponse.Close();
-				httpWebRequest.Abort();
-				StreamReader streamReader = new StreamReader(text, Encoding.Unicode);
-				string text2 = streamReader.ReadLine();
-				string text3 = streamReader.ReadLine();
-				string text4 = streamReader.ReadLine();
-				string text5 = streamReader.ReadLine();
-				streamReader.Close();
-				File.Delete(text);
-				if (text2 == null || text3 == null || text4 == null || text5 == null)
-				{
-					this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
-					this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
-					this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-					this.singleButton_Check.Enabled = true;
-					this.singleButton_Copy.Enabled = false;
-				}
-				else
-				{
-					string[] array2 = text2.Split(new char[]
-					{
-						'.'
-					});
-					if (array2.Length != 3)
-					{
-						this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
-						this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
-						this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-						this.singleButton_Check.Enabled = true;
-						this.singleButton_Copy.Enabled = false;
-					}
-					else
-					{
-						array2 = text3.Split(new char[]
-						{
-							'.'
-						});
-						if (array2.Length != 3)
-						{
-							this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
-							this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
-							this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-							this.singleButton_Check.Enabled = true;
-							this.singleButton_Copy.Enabled = false;
-						}
-						else if (text4 == "" || text5.Length < 32)
-						{
-							this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
-							this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
-							this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-							this.singleButton_Check.Enabled = true;
-							this.singleButton_Copy.Enabled = false;
-						}
-						else
-						{
-							this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : " + text2;
-							this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : " + text3;
-							this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : " + text4;
-							this.m_szDownloadLink = text5;
-							this.singleButton_Check.Enabled = true;
-							this.singleButton_Copy.Enabled = true;
-						}
-					}
-				}
-			}
-			catch
-			{
-				File.Delete(text);
-				this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
-				this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
-				this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
-				this.singleButton_Check.Enabled = true;
-				this.singleButton_Copy.Enabled = false;
-			}
-		}
-
-		// Token: 0x06000258 RID: 600 RVA: 0x0001EE20 File Offset: 0x0001D020
-		private void singleButton_Copy_ButtonClickNotify(SingleButton objSender)
-		{
-			Clipboard.Clear();
-			Clipboard.SetText(this.m_szDownloadLink);
-		}
 
 		// Token: 0x040002B7 RID: 695
 		private const string m_szCheckUpdateLink = "http://vipersaudio.com/swupdate/viper4windows/version.bin";
 
 		// Token: 0x040002B8 RID: 696
 		private string m_szDownloadLink = "";
-	}
+
+        private void SingleButton_Check_Click(object sender, EventArgs e)
+        {
+            string text = AppDomain.CurrentDomain.BaseDirectory;
+            if (!text.EndsWith("\\"))
+            {
+                text += "\\version.txt";
+            }
+            else
+            {
+                text += "version.txt";
+            }
+            try
+            {
+                this.singleButton_Check.Enabled = false;
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://vipersaudio.com/swupdate/viper4windows/version.bin");
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = httpWebResponse.GetResponseStream();
+                Stream stream = new FileStream(text, FileMode.Create);
+                byte[] array = new byte[1024];
+                for (int i = responseStream.Read(array, 0, array.Length); i > 0; i = responseStream.Read(array, 0, array.Length))
+                {
+                    stream.Write(array, 0, i);
+                }
+                stream.Flush();
+                stream.Close();
+                responseStream.Close();
+                httpWebResponse.Close();
+                httpWebRequest.Abort();
+                StreamReader streamReader = new StreamReader(text, Encoding.Unicode);
+                string text2 = streamReader.ReadLine();
+                string text3 = streamReader.ReadLine();
+                string text4 = streamReader.ReadLine();
+                string text5 = streamReader.ReadLine();
+                streamReader.Close();
+                File.Delete(text);
+                if (text2 == null || text3 == null || text4 == null || text5 == null)
+                {
+                    this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
+                    this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
+                    this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
+                    this.singleButton_Check.Enabled = true;
+                    this.singleButton_Copy.Enabled = false;
+                }
+                else
+                {
+                    string[] array2 = text2.Split(new char[]
+                    {
+                        '.'
+                    });
+                    if (array2.Length != 3)
+                    {
+                        this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
+                        this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
+                        this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
+                        this.singleButton_Check.Enabled = true;
+                        this.singleButton_Copy.Enabled = false;
+                    }
+                    else
+                    {
+                        array2 = text3.Split(new char[]
+                        {
+                            '.'
+                        });
+                        if (array2.Length != 3)
+                        {
+                            this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
+                            this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
+                            this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
+                            this.singleButton_Check.Enabled = true;
+                            this.singleButton_Copy.Enabled = false;
+                        }
+                        else if (text4 == "" || text5.Length < 32)
+                        {
+                            this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
+                            this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
+                            this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
+                            this.singleButton_Check.Enabled = true;
+                            this.singleButton_Copy.Enabled = false;
+                        }
+                        else
+                        {
+                            this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : " + text2;
+                            this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : " + text3;
+                            this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : " + text4;
+                            this.m_szDownloadLink = text5;
+                            this.singleButton_Check.Enabled = true;
+                            this.singleButton_Copy.Enabled = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                File.Delete(text);
+                this.label_ControlPanelVersion_Net.Text = GlobalMessages.CONTROL_PANEL_VERSION + " : N/A";
+                this.label_DriverVerion_Net.Text = GlobalMessages.DRIVER_VERSION + " : N/A";
+                this.label_CodeName_Net.Text = GlobalMessages.CODE_NAME + " : N/A";
+                this.singleButton_Check.Enabled = true;
+                this.singleButton_Copy.Enabled = false;
+            }
+        }
+
+        private void SingleButton_Copy_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(this.m_szDownloadLink);
+        }
+    }
 }
